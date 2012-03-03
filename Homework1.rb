0d1e0b286477adabe73ca67656c4e39a9687a720
salinda-kuapongthai <salinda.kuapongthai@gmail.com>
2012-03-03 18:43:13 +0000

    HW1_5

--- Homework1.rb
+++ Homework1.rb
@@ -1,3 +1,34 @@
+class Class
+  def attr_accessor_with_history(attr_name)
+    attr_name = attr_name.to_s
+    attr_reader attr_name
+    attr_reader attr_name+"_history"
+    class_eval %Q{
+      def #{attr_name}
+        @#{attr_name}
+      end
+      def #{attr_name}=(value)
+        if @#{attr_name+"_history"}==nil
+          @#{attr_name+"_history"} = [nil]
+        end
+        @#{attr_name+"_history"} += [value]
+        @#{attr_name} = value
+      end
+      def #{attr_name+"_history"}
+        @#{attr_name+"_history"}
+      end
+    }
+  end
+end
+
+class Foo
+  attr_accessor_with_history :bar
+end
+
+f = Foo.new
+f.bar = 1
+f.bar = 2
+f.bar_history
 
 def palindrome?(string)
  string = string.gsub(/[^a-zA-Z0-9]/, "")
