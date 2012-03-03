391b62a0c8ce2d13343d843633d9437c399f51c5
salinda-kuapongthai <salinda.kuapongthai@gmail.com>
2012-03-03 17:40:01 +0000

    HW1_4

--- Homework1.rb
+++ Homework1.rb
@@ -77,3 +77,67 @@ def combine_anagrams(words)
 end
 
 combine_anagrams(["cars","for","potatoes","racs","four","scar","creams","scream"])
+
+class Dessert
+  def initialize(name, calories)
+    @name = name
+    @calories = calories
+  end
+  def name
+    @name
+  end
+  def name=(value)
+    @name = value
+  end
+  def calories
+    @calories
+  end
+  def calories=(value)
+    @calories = value
+  end
+  def healthy?
+    if @calories < 200
+      true
+    else
+      false
+    end
+  end
+  def delicious?
+    true
+  end
+end
+
+class JellyBean < Dessert
+  def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+  def flavor
+    @flavor
+  end
+  def flavor=(value)
+    @flavor = value
+  end
+  def delicious?
+    if @flavor == "black licorice"
+      false
+    else
+      super()
+    end
+  end
+end
+
+dessert = Dessert.new("Cake", 250)
+dessert.delicious?
+dessert.healthy?
+dessert.name
+dessert.calories
+dessert.name = "cookies"
+dessert.calories = "100"
+dessert.name
+dessert.calories
+jellybean = JellyBean.new("JellyBean", 180, "black licorice")
+jellybean.delicious?
+jellybean.healthy?
+jellybean.flavor = "vanilla"
+jellybean.delicious?
