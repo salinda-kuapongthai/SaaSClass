2f88a1fefd4373fc28dad9dd5b069d988cd460af
salinda-kuapongthai <salinda.kuapongthai@gmail.com>
2012-03-03 16:06:45 +0000

    HW1_2

--- Homework1.rb
+++ Homework1.rb
@@ -20,4 +20,34 @@ def count_words(string)
  return hash
 end
 count_words("A man, a plan a canal -- Panama")
-
+class WrongNumberOfPlayersError < StandardError; end
+class NoSuchStrategyError < StandardError; end
+def rps_game_winner(game)
+ raise WrongNumberOfPlayersError unless game.length == 2
+ firstPlayer = game[0][1].downcase
+ secondPlayer = game[1][1].downcase
+ raise NoSuchStrategyError unless ["s","p","r"].include?(firstPlayer)
+ raise NoSuchStrategyError unless ["s","p","r"].include?(secondPlayer)
+ if firstPlayer == "s" && secondPlayer == "p"
+  return game[0]
+ elsif firstPlayer == "p" && secondPlayer == "r"
+  return game[0]
+ elsif firstPlayer == "r" && secondPlayer == "s"
+  return game[0]
+ elsif firstPlayer == secondPlayer
+  return game[0]
+ else
+  return game[1]
+ end
+end
+rps_game_winner([["Da","P"],["Someone","S"]])
+rps_game_winner([["Da","s"]])
+rps_game_winner([["Da","0"],["Someone","p"]])
+def rps_tournament_winner(tournament)
+ if tournament.length == 2 && !tournament[0].kind_of?(Array) && !tournament[1].kind_of?(Array)
+  return tournament
+ else
+  return rps_game_winner([rps_tournament_winner(tournament[0]),rps_tournament_winner(tournament[1])])
+ end
+end
+rps_tournament_winner([[[["Armando","P"],["Dave","S"]],[["Richard","R"],["Michael","S"]]],[[["Allen","S"],["Omer","P"]],[["David E.","R"],["Richaed X.","P"]]]])
