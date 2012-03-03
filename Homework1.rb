683a4281490fff4b03317654741b4cd16d166182
salinda-kuapongthai <salinda.kuapongthai@gmail.com>
2012-03-03 13:13:31 +0000

    HW1_b

--- Homework1.rb
+++ Homework1.rb
@@ -10,3 +10,14 @@ palindrome?("A man, a plan, a canal -- Panama")
 palindrome?("Madam, I'm Adam!")
 palindrome?("Abracadabra")
 
+def count_words(string)
+ string = string.split(/[^a-zA-Z0-9]/)
+ string.delete_if{|s| s==""}
+ string =  string.collect{|s| s.downcase}
+ hash = Hash.new
+ string.each do |s| hash[s] = 0 end
+ string.each do |s| hash[s] = hash[s] + 1 end
+ return hash
+end
+count_words("A man, a plan a canal -- Panama")
+
