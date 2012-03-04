cde8f8912a01269ea532ab10167c1750ca1e560f
salinda-kuapongthai <salinda.kuapongthai@gmail.com>
2012-03-04 22:36:55 +0700

    fix HW1_3

--- Homework1_3.rb
+++ Homework1_3.rb
@@ -2,7 +2,8 @@ def combine_anagrams(words)
   group = []
   hash = Hash.new
   words.each do |word|
-   sortStr = word.chars.sort.join.downcase
+   sortStr = word.downcase 
+   sortStr = sortStr.chars.sort.join
    if hash[sortStr] == nil
     hash[sortStr] =  [word]
    else
